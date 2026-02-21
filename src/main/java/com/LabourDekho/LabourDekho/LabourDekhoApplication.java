85a5a51b2b10412f61cb1040020e123856fb318b
rahul sinha1 <rahul.sinha1@loblaw.ca>
2026-02-21 17:15:22 +0530

    initial commit

---
+++ src/main/java/com/LabourDekho/LabourDekho/LabourDekhoApplication.java
@@ -0,0 +1,13 @@
+package com.LabourDekho.LabourDekho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabourDekhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabourDekhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LabourDekho/LabourDekho/LabourDekhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LabourDekho.LabourDekho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabourDekhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
