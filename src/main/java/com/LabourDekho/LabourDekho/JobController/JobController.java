a1f0ceb11b4350aeff18fd9bac3ff3e329f91b7b
rahul sinha1 <rahul.sinha1@loblaw.ca>
2026-02-24 15:18:04 +0530

    added demo controller

--- src/main/java/com/LabourDekho/LabourDekho/JobController/JobController.java
+++ src/main/java/com/LabourDekho/LabourDekho/JobController/JobController.java
@@ -1,3 +1,9 @@
+package com.LabourDekho.LabourDekho.JobController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api")
 public class JobController {
